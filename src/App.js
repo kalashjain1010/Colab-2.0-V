365a3ad58afd00595d867084b8d60351d17e6547
kalash jain <kalashjain54@gmail.com>
2023-06-24 11:55:15 +0530

    adding landing page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import * as React from 'react'
 
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <ChakraProvider>
       <Navbar/>
+      <LandingPage/>
     </ChakraProvider>
   );
 }
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./css/landingPage.css";
+
+function LandingPage() {
+  return (
+    <div className="landing">
+      <div className="heading">Landing page</div>
+      <div className="subhead">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quisquam
+        maiores deserunt cupiditate totam hic necessitatibus? Iure dicta sit
+        officia unde, reprehenderit, sed at tenetur fuga nihil earum fugiat
+        nostrum. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Quisquam mollitia iusto, officiis excepturi cum odio doloremque beatae
+        voluptatum dignissimos, sit perspiciatis itaque at modi harum eius nisi
+        nam minima hic! Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Ratione maxime atque nobis obcaecati suscipit molestias corporis
+        voluptates animi possimus explicabo. Consectetur recusandae quo corporis
+        voluptatem, possimus iste blanditiis explicabo error.
+
+        <div className="started">
+            <button className="getStarted">get started</button>
+        </div>
+      </div>
+      <div className="landingImage">
+        image
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
